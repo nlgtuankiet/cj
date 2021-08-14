4091a4b9a6dd2b78e7a84a898d861279eff8ce7f
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-08-14 12:32:58 +0700

    setup config activity + provider

---
+++ app/src/main/java/com/rainyseason/cj/ticker/CoinTickerProvider.kt
@@ -0,0 +1,21 @@
+package com.rainyseason.cj.ticker
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+
+class CoinTickerProvider : AppWidgetProvider() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+    }
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+}
--- app/src/main/java/com/rainyseason/cj/ticker/CoinTickerSettingActivity.kt
+++ app/src/main/java/com/rainyseason/cj/ticker/CoinTickerSettingActivity.kt
@@ -1,17 +1,19 @@
-package com.rainyseason.cj
+package com.rainyseason.cj.ticker
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.widget.RemoteViews
 import androidx.appcompat.app.AppCompatActivity
+import com.rainyseason.cj.LocalRemoteViews
+import com.rainyseason.cj.R
 
 class CoinTickerSettingActivity : AppCompatActivity() {
 
     lateinit var remoteView: RemoteViews
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setResult(RESULT_CANCELED)
         setContentView(R.layout.activity_coin_ticker_setting)
-
+        println("CoinTickerSettingActivity: onCreate")
         remoteView = LocalRemoteViews(this, R.id.preview_container, R.layout.widget_coin_ticker)
     }
 }
