36b9fb02f2e9495a2b7adb5b2f0d0407ae376b09
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-09-11 09:54:14 +0700

    fix refresh all button

--- app/src/main/java/com/rainyseason/cj/RefreshReceiver.kt
+++ app/src/main/java/com/rainyseason/cj/RefreshReceiver.kt
@@ -6,7 +6,9 @@ import android.content.Context
 import android.content.Intent
 import com.rainyseason.cj.common.coreComponent
 import com.rainyseason.cj.common.goBackground
-import com.rainyseason.cj.ticker.CoinTickerProvider
+import com.rainyseason.cj.ticker.CoinTickerProviderCoin360
+import com.rainyseason.cj.ticker.CoinTickerProviderDefault
+import com.rainyseason.cj.ticker.CoinTickerProviderGraph
 
 class RefreshReceiver : BroadcastReceiver() {
     private val actions = listOf(
@@ -28,10 +30,16 @@ class RefreshReceiver : BroadcastReceiver() {
         // refresh add ticker widget
         val widgetManager = context.coreComponent.appWidgetManager
         val tickerHandler = context.coreComponent.coinTickerHandler
-        val tickerComponent = ComponentName(context, CoinTickerProvider::class.java)
-        val widgetIds = widgetManager.getAppWidgetIds(tickerComponent)
-        widgetIds.forEach {
-            tickerHandler.enqueueRefreshWidget(it)
+
+        listOf(
+            CoinTickerProviderDefault::class.java,
+            CoinTickerProviderCoin360::class.java,
+            CoinTickerProviderGraph::class.java,
+        ).flatMap { clazz ->
+            val tickerComponent = ComponentName(context, clazz)
+            widgetManager.getAppWidgetIds(tickerComponent).toList()
+        }.forEach { widgetId ->
+            tickerHandler.enqueueRefreshWidget(widgetId)
         }
     }
 }
