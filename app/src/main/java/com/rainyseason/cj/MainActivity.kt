45f4e2c9da3c1b17b0afb2442a434152d2a692b7
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-09-11 10:07:33 +0700

    add open telegram button

--- app/src/main/java/com/rainyseason/cj/MainActivity.kt
+++ app/src/main/java/com/rainyseason/cj/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.rainyseason.cj
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.rainyseason.cj.data.coingecko.CoinGeckoService
 import dagger.Module
 import dagger.android.AndroidInjection
 import dagger.android.ContributesAndroidInjector
 import javax.inject.Inject
+
 
 @Module
 interface MainActivityModule {
@@ -23,5 +27,13 @@ class MainActivity : AppCompatActivity() {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.open_telegram).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("tg://resolve?domain=bwpapp")
+            startActivity(intent)
+        }
     }
+
+
+
 }
