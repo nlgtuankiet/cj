8a02a0dabc0c52278cc8d087df5e5bfd8ce8d840
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-09-14 10:50:20 +0700

    correct launch script

--- launch_preview.sh
+++ launch_preview.sh
@@ -1,4 +1,5 @@
 gradle :app:installDebug
-adb shell am start -n com.rainyseason.cj/.ticker.CoinTickerSettingActivity \
+adb shell am start -n "com.rainyseason.cj/.ticker.CoinTickerSettingActivity" \
+  -a android.intent.action.MAIN -c android.intent.category.LAUNCHER \
   --ei appWidgetId 123 \
   --es coin_id bitcoin
--- set_flag.sh
+++ set_flag.sh
@@ -3,7 +3,8 @@ value_value=$2
 rand=$(head -n 4096 /dev/urandom | openssl sha1)
 
 echo "set $key_value to $value_value rand $rand"
-adb shell am start -n com.rainyseason.cj/.featureflag.DebugFlagSetter \
+adb shell am start -n "com.rainyseason.cj/com.rainyseason.cj.featureflag.DebugFlagSetter" \
+  -a android.intent.action.MAIN -c android.intent.category.LAUNCHER \
   --es key "$key_value" \
   --es value "$value_value" \
   --es rand "$rand"
