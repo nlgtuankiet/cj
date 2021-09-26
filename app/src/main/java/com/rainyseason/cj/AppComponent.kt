7d146780035decba90e011fd0726a4d2572190d9
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-09-26 16:51:10 +0700

    fix build fail

--- app/src/main/java/com/rainyseason/cj/AppComponent.kt
+++ app/src/main/java/com/rainyseason/cj/AppComponent.kt
@@ -14,6 +14,7 @@ import androidx.datastore.preferences.preferencesDataStoreFile
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.perf.FirebasePerformance
 import com.rainyseason.cj.common.CoinTickerStorage
 import com.rainyseason.cj.common.CoreComponent
@@ -116,6 +117,12 @@ object AppProvides {
             }
         }
         return builder
+    }
+
+    @Provides
+    @Singleton
+    fun firebaseCrashlytic(context: Context): FirebaseCrashlytics {
+        return FirebaseCrashlytics.getInstance()
     }
 
     @Provides
