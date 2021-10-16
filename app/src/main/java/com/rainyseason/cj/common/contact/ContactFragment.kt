728cf34843c2665c456e64eb37fc24fd62add3d9
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-10-16 13:24:57 +0700

    refine contact ui

--- app/src/main/java/com/rainyseason/cj/common/contact/ContactFragment.kt
+++ app/src/main/java/com/rainyseason/cj/common/contact/ContactFragment.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.rainyseason.cj.R
 
@@ -13,7 +12,8 @@ class ContactFragment : Fragment(R.layout.fragment_contact) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.open_telegram).setOnClickListener {
+        val binding = FrlagmentContactBinding.bind(view)
+        binding.openTelegram.setOnClickListener {
             try {
                 val intent = Intent(Intent.ACTION_VIEW)
                 intent.data = Uri.parse("tg://resolve?domain=bwpapp")
@@ -23,6 +23,29 @@ class ContactFragment : Fragment(R.layout.fragment_contact) {
                 intent.data = Uri.parse("https://t.me/bwpapp")
                 startActivity(intent)
             }
+        }
+
+        binding.openReddit.setOnClickListener {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("https://www.reddit.com/r/bwp/")
+                startActivity(intent)
+            } catch (ex: ActivityNotFoundException) {
+
+            }
+        }
+
+        binding.openMail.setOnClickListener {
+            try {
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("mailto:")
+                intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("nlg.tuan.kiet@gmail.com"))
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Hello!")
+                startActivity(intent)
+            } catch (ignored: Exception) {
+
+            }
+
         }
     }
 }
