6bd05e07c1a0bf2b69c78b69dc91a2aa53435eb7
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2021-12-03 18:57:42 +0700

    async get coin detail

--- app/src/main/java/com/rainyseason/cj/ticker/usecase/GetCoinGeckoDisplayData.kt
+++ app/src/main/java/com/rainyseason/cj/ticker/usecase/GetCoinGeckoDisplayData.kt
@@ -5,6 +5,8 @@ import com.rainyseason.cj.common.model.asDayString
 import com.rainyseason.cj.data.coingecko.CoinGeckoService
 import com.rainyseason.cj.data.coingecko.currentPrice
 import com.rainyseason.cj.ticker.CoinTickerDisplayData
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
 
 class GetCoinGeckoDisplayData @Inject constructor(
@@ -14,50 +16,53 @@ class GetCoinGeckoDisplayData @Inject constructor(
     suspend operator fun invoke(
         param: CoinTickerDisplayData.LoadParam
     ): CoinTickerDisplayData {
-        val coinDetail = coinGeckoService.getCoinDetail(param.coinId)
-        val graphResponse = coinGeckoService.getMarketChart(
-            id = param.coinId,
-            vsCurrency = param.currency,
-            day = param.changeInterval.asDayString()!!
-        )
-
-        val marketPrice = if (param.changeInterval == TimeInterval.I_24H) {
-            graphResponse.currentPrice()
-        } else {
-            coinGeckoService.getMarketChart(
+        return coroutineScope {
+            val coinDetailAsync = async { coinGeckoService.getCoinDetail(param.coinId) }
+            val graphResponse = coinGeckoService.getMarketChart(
                 id = param.coinId,
                 vsCurrency = param.currency,
-                day = "1",
-            ).currentPrice()
+                day = param.changeInterval.asDayString()!!
+            )
+
+            val marketPrice = if (param.changeInterval == TimeInterval.I_24H) {
+                graphResponse.currentPrice()
+            } else {
+                coinGeckoService.getMarketChart(
+                    id = param.coinId,
+                    vsCurrency = param.currency,
+                    day = "1",
+                ).currentPrice()
+            }
+
+            val coinDetail = coinDetailAsync.await()
+            val price = marketPrice ?: coinDetail.marketData.currentPrice[param.currency]
+
+            val priceChangePercent = when (param.changeInterval) {
+                TimeInterval.I_7D ->
+                    coinDetail.marketData
+                        .priceChangePercentage7dInCurrency[param.currency]
+                TimeInterval.I_14D ->
+                    coinDetail.marketData
+                        .priceChangePercentage14dInCurrency[param.currency]
+                TimeInterval.I_30D ->
+                    coinDetail.marketData
+                        .priceChangePercentage30dInCurrency[param.currency]
+                TimeInterval.I_1Y ->
+                    coinDetail.marketData
+                        .priceChangePercentage1yInCurrency[param.currency]
+                else ->
+                    coinDetail.marketData
+                        .priceChangePercentage24hInCurrency[param.currency]
+            }
+
+            CoinTickerDisplayData(
+                iconUrl = coinDetail.image.large,
+                symbol = coinDetail.symbol,
+                name = coinDetail.name,
+                price = price,
+                priceChangePercent = priceChangePercent,
+                priceGraph = graphResponse.prices,
+            )
         }
-
-        val price = marketPrice ?: coinDetail.marketData.currentPrice[param.currency]
-
-        val priceChangePercent = when (param.changeInterval) {
-            TimeInterval.I_7D ->
-                coinDetail.marketData
-                    .priceChangePercentage7dInCurrency[param.currency]
-            TimeInterval.I_14D ->
-                coinDetail.marketData
-                    .priceChangePercentage14dInCurrency[param.currency]
-            TimeInterval.I_30D ->
-                coinDetail.marketData
-                    .priceChangePercentage30dInCurrency[param.currency]
-            TimeInterval.I_1Y ->
-                coinDetail.marketData
-                    .priceChangePercentage1yInCurrency[param.currency]
-            else ->
-                coinDetail.marketData
-                    .priceChangePercentage24hInCurrency[param.currency]
-        }
-
-        return CoinTickerDisplayData(
-            iconUrl = coinDetail.image.large,
-            symbol = coinDetail.symbol,
-            name = coinDetail.name,
-            price = price,
-            priceChangePercent = priceChangePercent,
-            priceGraph = graphResponse.prices,
-        )
     }
 }
