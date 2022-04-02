772d901cd5fcc6092adfec13c0c218385cd9eb56
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2022-04-02 15:16:43 +0700

    fix crash sample activity

--- app/src/main/java/com/rainyseason/cj/DrawSampleActivity.kt
+++ app/src/main/java/com/rainyseason/cj/DrawSampleActivity.kt
@@ -1,16 +1,12 @@
 package com.rainyseason.cj
 
 import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.rainyseason.cj.common.getColorCompat
+import com.amplitude.api.AmplitudeClient
 import com.rainyseason.cj.data.cmc.CmcService
 import dagger.Module
 import dagger.android.AndroidInjection
 import dagger.android.ContributesAndroidInjector
-import timber.log.Timber
 import javax.inject.Inject
 
 @Module
@@ -24,93 +20,17 @@ class DrawSampleActivity : AppCompatActivity() {
     @Inject
     lateinit var cmcService: CmcService
 
+    @Inject
+    lateinit var amplitudeClient: AmplitudeClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidInjection.inject(this)
         setContentView(R.layout.activity_draw_sample)
-        listOf(
-            android.R.color.system_accent1_0,
-            android.R.color.system_accent1_10,
-            android.R.color.system_accent1_50,
-            android.R.color.system_accent1_100,
-            android.R.color.system_accent1_200,
-            android.R.color.system_accent1_300,
-            android.R.color.system_accent1_400,
-            android.R.color.system_accent1_500,
-            android.R.color.system_accent1_600,
-            android.R.color.system_accent1_700,
-            android.R.color.system_accent1_800,
-            android.R.color.system_accent1_900,
-            android.R.color.system_accent1_1000,
-
-            android.R.color.system_accent2_0,
-            android.R.color.system_accent2_10,
-            android.R.color.system_accent2_50,
-            android.R.color.system_accent2_100,
-            android.R.color.system_accent2_200,
-            android.R.color.system_accent2_300,
-            android.R.color.system_accent2_400,
-            android.R.color.system_accent2_500,
-            android.R.color.system_accent2_600,
-            android.R.color.system_accent2_700,
-            android.R.color.system_accent2_800,
-            android.R.color.system_accent2_900,
-            android.R.color.system_accent2_1000,
-
-            android.R.color.system_accent3_0,
-            android.R.color.system_accent3_10,
-            android.R.color.system_accent3_50,
-            android.R.color.system_accent3_100,
-            android.R.color.system_accent3_200,
-            android.R.color.system_accent3_300,
-            android.R.color.system_accent3_400,
-            android.R.color.system_accent3_500,
-            android.R.color.system_accent3_600,
-            android.R.color.system_accent3_700,
-            android.R.color.system_accent3_800,
-            android.R.color.system_accent3_900,
-            android.R.color.system_accent3_1000,
-
-            android.R.color.system_neutral1_0,
-            android.R.color.system_neutral1_10,
-            android.R.color.system_neutral1_50,
-            android.R.color.system_neutral1_100,
-            android.R.color.system_neutral1_200,
-            android.R.color.system_neutral1_300,
-            android.R.color.system_neutral1_400,
-            android.R.color.system_neutral1_500,
-            android.R.color.system_neutral1_600,
-            android.R.color.system_neutral1_700,
-            android.R.color.system_neutral1_800,
-            android.R.color.system_neutral1_900,
-            android.R.color.system_neutral1_1000,
-
-            android.R.color.system_neutral2_0,
-            android.R.color.system_neutral2_10,
-            android.R.color.system_neutral2_50,
-            android.R.color.system_neutral2_100,
-            android.R.color.system_neutral2_200,
-            android.R.color.system_neutral2_300,
-            android.R.color.system_neutral2_400,
-            android.R.color.system_neutral2_500,
-            android.R.color.system_neutral2_600,
-            android.R.color.system_neutral2_700,
-            android.R.color.system_neutral2_800,
-            android.R.color.system_neutral2_900,
-            android.R.color.system_neutral2_1000,
-        ).forEach {
-            val container = findViewById<LinearLayout>(R.id.content)
-            val view = View(this)
-            view.layoutParams = LinearLayout.LayoutParams(200, 200)
-            view.setBackgroundColor(getColorCompat(it))
-            container.addView(view)
-            val text = TextView(this)
-            text.text = resources.getResourceEntryName(it)
-            container.addView(text)
-            Timber.d(
-                "Res ${resources.getResourceEntryName(it)} is " +
-                    "${java.lang.Integer.toHexString(getColorCompat(it))}}"
-            )
+        if (!BuildConfig.DEBUG) {
+            finish()
+            return
         }
+        amplitudeClient.logEvent("hello_world")
     }
 }
